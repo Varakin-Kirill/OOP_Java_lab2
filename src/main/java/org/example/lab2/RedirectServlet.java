7881341dd2a7ec993cb0a407332af4d1c9e4d14f
Varakin-Kirill <kirilld1122@gmail.com>
2024-05-15 01:21:06 +0600

    commit

---
+++ src/main/java/org/example/lab2/RedirectServlet.java
@@ -0,0 +1,20 @@
+package org.example.lab2;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet(name = "RedirectServlet", urlPatterns = {"/services", "/contact", "/feedback","/about"})
+public class RedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String path = request.getServletPath();
+        String targetPage = path.substring(1) + ".html";
+
+        response.setStatus(response.SC_ACCEPTED);
+        response.setHeader("Location", targetPage);
+        response.sendRedirect(targetPage);
+    }
+}
